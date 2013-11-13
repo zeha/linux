bc0676b0251b871ee4ac37e3c8a29ec930770628
Lena Salman <esalman@codeaurora.org>
2013-11-13 13:59:27 +0200

    usb: gadget: ci13xxx_udc: Access dev only after NULL check
    Fix various code quality issues found by the automatic checking tool.
    We now access dev fields only after we verify it's not NULL.
    
    Change-Id: I5c43090049437b88941c72de96defdc36368fca1
    CRs-Fixed: 566927
    Signed-off-by: Danny Segal <dsegal@codeaurora.org>
    Signed-off-by: Lena Salman <esalman@codeaurora.org>
    Signed-off-by: ykong <ykong@sierrawireless.com>

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -3684,8 +3684,7 @@ static int udc_probe(struct ci13xxx_udc_driver *driver, struct device *dev,
 		void __iomem *regs)
 {
 	struct ci13xxx *udc;
-	struct ci13xxx_platform_data *pdata =
-		(struct ci13xxx_platform_data *)(dev->platform_data);
+	struct ci13xxx_platform_data *pdata;
 	int retval = 0, i;
 
 	trace("%p, %p, %p", dev, regs, driver->name);
@@ -3697,6 +3696,8 @@ static int udc_probe(struct ci13xxx_udc_driver *driver, struct device *dev,
 	udc = kzalloc(sizeof(struct ci13xxx), GFP_KERNEL);
 	if (udc == NULL)
 		return -ENOMEM;
+
+	pdata = (struct ci13xxx_platform_data *)(dev->platform_data);
 
 	udc->lock = &udc_lock;
 	udc->regs = regs;
