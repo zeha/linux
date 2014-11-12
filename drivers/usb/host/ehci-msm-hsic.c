5a7d600b7ca8685d2a0acb8fa886d689cd29ffb1
Zoran Markovic <zmarkovic@sierrawireless.com>
2014-11-12 11:17:15 -0800

    ehci-msm-hsic: Use internal wakeup_source
    The external wake_lock structure was mis-used by releasing it in
    _suspend() routine and grabbing it in _resume(). This prevented
    the system to enter auto-sleep.
    
    Convert the driver to use the device's internal wakeup_source
    instead of adding another to its private block. Use wakeup_source
    appropriately so as not to prevent the system from auto-sleep and
    allow a timeout interval for user-space to process a device event.
    
    Change-Id: I53f1bec95fb95fe7c0227847507f410fb6422486
    Signed-off-by: Zoran Markovic <zmarkovic@sierrawireless.com>

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -82,7 +82,6 @@ struct msm_hsic_hcd {
 	struct regulator	*hsic_gdsc;
 	atomic_t			async_int;
 	atomic_t                in_lpm;
-	struct wake_lock	wlock;
 	int			peripheral_status_irq;
 	int			wakeup_irq;
 	bool			wakeup_irq_enabled;
@@ -750,7 +749,6 @@ static int msm_hsic_suspend(struct msm_hsic_hcd *mehci)
 			PM_QOS_DEFAULT_VALUE);
 #endif
 /* SWISTOP */
-	wake_unlock(&mehci->wlock);
 
 	dev_info(mehci->dev, "HSIC-USB in low power mode\n");
 
@@ -792,8 +790,6 @@ static int msm_hsic_resume(struct msm_hsic_hcd *mehci)
 	   }
 	   spin_unlock_irqrestore(&mehci->wakeup_lock, flags);
 	}
-
-	wake_lock(&mehci->wlock);
 
 	if (mehci->bus_perf_client && debug_bus_voting_enabled) {
 		mehci->bus_vote = true;
@@ -1352,7 +1348,8 @@ static irqreturn_t msm_hsic_wakeup_irq(int irq, void *data)
 	dev_dbg(mehci->dev, "%s: hsic remote wakeup interrupt %d cnt: %u, %u\n",
 		    __func__, irq, mehci->wakeup_int_cnt, mehci->async_int_cnt);
 
-	wake_lock(&mehci->wlock);
+	/* allow 200 ms for userspace to process event */
+	pm_wakeup_event(mehci->dev, 200);
 
 	if (mehci->wakeup_irq) {
 		spin_lock(&mehci->wakeup_lock);
@@ -1689,8 +1686,6 @@ static int ehci_hsic_msm_probe(struct platform_device *pdev)
 	}
 
 	device_init_wakeup(&pdev->dev, 1);
-	wake_lock_init(&mehci->wlock, WAKE_LOCK_SUSPEND, dev_name(&pdev->dev));
-	wake_lock(&mehci->wlock);
 
 	if (mehci->peripheral_status_irq) {
 		ret = request_threaded_irq(mehci->peripheral_status_irq,
@@ -1844,7 +1839,6 @@ static int ehci_hsic_msm_remove(struct platform_device *pdev)
 	msm_hsic_init_vddcx(mehci, 0);
 
 	msm_hsic_init_clocks(mehci, 0);
-	wake_lock_destroy(&mehci->wlock);
 	iounmap(hcd->regs);
 	usb_put_hcd(hcd);
 
