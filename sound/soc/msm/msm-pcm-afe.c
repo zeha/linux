b89fbfd5268795f35a55200ecf026fd2bcf80070
Mihai Dragusu <mihai.dragusu@windriver.com>
2015-03-02 14:29:30 +0200

    fix compilation: remove __dev*
    Signed-off-by: Mihai Dragusu <mihai.dragusu@windriver.com>

--- sound/soc/msm/msm-pcm-afe.c
+++ sound/soc/msm/msm-pcm-afe.c
@@ -582,7 +582,7 @@ static struct snd_soc_platform_driver msm_soc_platform = {
 	.probe		= msm_afe_afe_probe,
 };
 
-static __devinit int msm_afe_probe(struct platform_device *pdev)
+static  int msm_afe_probe(struct platform_device *pdev)
 {
 	pr_debug("%s: dev name %s\n", __func__, dev_name(&pdev->dev));
 	return snd_soc_register_platform(&pdev->dev,
@@ -602,7 +602,7 @@ static struct platform_driver msm_afe_driver = {
 		.owner = THIS_MODULE,
 	},
 	.probe = msm_afe_probe,
-	.remove = __devexit_p(msm_afe_remove),
+	.remove = msm_afe_remove,
 };
 
 static int __init msm_soc_platform_init(void)
