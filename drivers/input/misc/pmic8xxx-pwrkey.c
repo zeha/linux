5c5453f102619c825dcecef55d1c7adfede386e1
Dragos Minuta <dragos.minuta@windriver.com>
2015-04-27 14:57:57 +0300

    Fix pmic8xxx-pwrkey warning
    Signed-off-by: Dragos Minuta <dragos.minuta@windriver.com>

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -11,13 +11,13 @@
  */
 
 #include <linux/module.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
-#include <linux/regmap.h>
 #include <linux/log2.h>
 
 #include <linux/mfd/pm8xxx/core.h>
@@ -45,22 +45,22 @@ struct pmic8xxx_pwrkey {
 	const struct pm8xxx_pwrkey_platform_data *pdata;
 };
 
-static irqreturn_t pwrkey_press_irq(int irq, void *_pwr)
+static irqreturn_t pwrkey_press_irq(int irq, void *_pwrkey)
 {
-	struct input_dev *pwr = _pwr;
+	struct pmic8xxx_pwrkey *pwrkey = _pwrkey;
 
-	input_report_key(pwr, KEY_POWER, 1);
-	input_sync(pwr);
+	input_report_key(pwrkey->pwr, KEY_POWER, 1);
+	input_sync(pwrkey->pwr);
 
 	return IRQ_HANDLED;
 }
 
-static irqreturn_t pwrkey_release_irq(int irq, void *_pwr)
+static irqreturn_t pwrkey_release_irq(int irq, void *_pwrkey)
 {
-	struct input_dev *pwr = _pwr;
+	struct pmic8xxx_pwrkey *pwrkey = _pwrkey;
 
-	input_report_key(pwr, KEY_POWER, 0);
-	input_sync(pwr);
+	input_report_key(pwrkey->pwr, KEY_POWER, 0);
+	input_sync(pwrkey->pwr);
 
 	return IRQ_HANDLED;
 }
@@ -94,14 +94,7 @@ static int pmic8xxx_set_pon1(struct device *dev, u32 debounce_us, bool pull_up)
 {
 	int err;
 	u32 delay;
-	unsigned pon_cntl;
-	struct regmap *regmap;
-
-	regmap = dev_get_regmap(dev->parent, NULL);
-	if (!regmap) {
-		dev_err(dev, "failed to locate regmap for the device\n");
-		return -ENODEV;
-	}
+	u8 pon_cntl;
 
 	/* Valid range of pwr key trigger delay is 1/64 sec to 2 seconds. */
 	if (debounce_us > USEC_PER_SEC * 2 ||
@@ -113,7 +106,7 @@ static int pmic8xxx_set_pon1(struct device *dev, u32 debounce_us, bool pull_up)
 	delay = (debounce_us << 6) / USEC_PER_SEC;
 	delay = ilog2(delay);
 
-	err = regmap_read(regmap, PON_CNTL_1, &pon_cntl);
+	err = pm8xxx_readb(dev->parent, PON_CNTL_1, &pon_cntl);
 	if (err < 0) {
 		dev_err(dev, "failed reading PON_CNTL_1 err=%d\n", err);
 		return err;
@@ -127,7 +120,7 @@ static int pmic8xxx_set_pon1(struct device *dev, u32 debounce_us, bool pull_up)
 	else
 		pon_cntl &= ~PON_CNTL_PULL_UP;
 
-	err = regmap_write(regmap, PON_CNTL_1, pon_cntl);
+	err = pm8xxx_writeb(dev->parent, PON_CNTL_1, pon_cntl);
 	if (err < 0) {
 		dev_err(dev, "failed writing PON_CNTL_1 err=%d\n", err);
 		return err;
@@ -158,7 +151,13 @@ static ssize_t pmic8xxx_debounce_store(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(debounce_us, 0664, NULL, pmic8xxx_debounce_store);
+static ssize_t pmic8xxx_debounce_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "pmic8xxx\n");
+}
+
+static DEVICE_ATTR(debounce_us, 0664, pmic8xxx_debounce_show, pmic8xxx_debounce_store);
 
 static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 {
@@ -175,45 +174,50 @@ static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	pwrkey = devm_kzalloc(&pdev->dev, sizeof(*pwrkey), GFP_KERNEL);
+	pwrkey = kzalloc(sizeof(*pwrkey), GFP_KERNEL);
 	if (!pwrkey)
 		return -ENOMEM;
 
-	pwrkey->key_press_irq = key_press_irq;
 	pwrkey->pdata = pdata;
 
-	pwr = devm_input_allocate_device(&pdev->dev);
+	pwr = input_allocate_device();
 	if (!pwr) {
 		dev_dbg(&pdev->dev, "Can't allocate power button\n");
-		return -ENOMEM;
+		err = -ENOMEM;
+		goto free_pwrkey;
 	}
 
 	input_set_capability(pwr, EV_KEY, KEY_POWER);
 
 	pwr->name = "pmic8xxx_pwrkey";
 	pwr->phys = "pmic8xxx_pwrkey/input0";
+	pwr->dev.parent = &pdev->dev;
 
 	err = pmic8xxx_set_pon1(&pdev->dev,
-				pdata->kpd_trigger_delay_us, pdata->pull_up);
+			pdata->kpd_trigger_delay_us, pdata->pull_up);
 	if (err) {
 		dev_dbg(&pdev->dev, "Can't set PON CTRL1 register: %d\n", err);
+		goto free_input_dev;
 	}
 
 	err = input_register_device(pwr);
 	if (err) {
-		dev_err(&pdev->dev, "Can't register power key: %d\n", err);
-		return err;
+		dev_dbg(&pdev->dev, "Can't register power key: %d\n", err);
+		goto free_input_dev;
 	}
 
+	pwrkey->key_press_irq = key_press_irq;
 	pwrkey->pwr = pwr;
 
 	platform_set_drvdata(pdev, pwrkey);
+
 /* SWISTART */
 #if defined(CONFIG_SIERRA_PWRKEY)
 	/* check power key status during boot */
 	err = pm8xxx_read_irq_stat(pdev->dev.parent, key_press_irq);
 	if (err < 0) {
 		dev_err(&pdev->dev, "reading irq status failed\n");
+		goto unreg_input_dev;
 	}
 	pwrkey->press = !!err;
 
@@ -225,17 +229,20 @@ static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 /* SWISTOP */
 
 	err = request_any_context_irq(key_press_irq, pwrkey_press_irq,
-			IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_press", pwrkey);
+		IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_press", pwrkey);
 	if (err < 0) {
 		dev_dbg(&pdev->dev, "Can't get %d IRQ for pwrkey: %d\n",
 				 key_press_irq, err);
+		goto unreg_input_dev;
 	}
 
 	err = request_any_context_irq(key_release_irq, pwrkey_release_irq,
-			 IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_release", pwrkey);
+		 IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_release", pwrkey);
 	if (err < 0) {
 		dev_dbg(&pdev->dev, "Can't get %d IRQ for pwrkey: %d\n",
 				 key_release_irq, err);
+
+		goto free_press_irq;
 	}
 
 	err = device_create_file(&pdev->dev, &dev_attr_debounce_us);
@@ -243,16 +250,42 @@ static int pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev,
 				"dev file creation for debounce failed: %d\n",
 				err);
+		goto free_rel_irq;
 	}
 
 	device_init_wakeup(&pdev->dev, pdata->wakeup);
 
 	return 0;
+
+free_rel_irq:
+	free_irq(key_release_irq, pwrkey);
+free_press_irq:
+	free_irq(key_press_irq, pwrkey);
+unreg_input_dev:
+	platform_set_drvdata(pdev, NULL);
+	input_unregister_device(pwr);
+	pwr = NULL;
+free_input_dev:
+	input_free_device(pwr);
+free_pwrkey:
+	kfree(pwrkey);
+	return err;
 }
 
 static int pmic8xxx_pwrkey_remove(struct platform_device *pdev)
 {
+	struct pmic8xxx_pwrkey *pwrkey = platform_get_drvdata(pdev);
+	int key_release_irq = platform_get_irq(pdev, 0);
+	int key_press_irq = platform_get_irq(pdev, 1);
+
 	device_init_wakeup(&pdev->dev, 0);
+
+	device_remove_file(&pdev->dev, &dev_attr_debounce_us);
+	free_irq(key_press_irq, pwrkey);
+	free_irq(key_release_irq, pwrkey);
+	input_unregister_device(pwrkey->pwr);
+	platform_set_drvdata(pdev, NULL);
+	kfree(pwrkey);
 
 	return 0;
 }
