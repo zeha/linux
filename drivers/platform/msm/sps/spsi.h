72e28c5cad3a4b75330e5d30bd900a3cd061bc43
Siva Kumar Akkireddi <sivaa@codeaurora.org>
2017-05-11 15:29:47 +0530

    msm: sps: Fix race condition in SPS debugfs APIs
    SPS debugfs APIs can be called concurrently which can result
    in dangling pointer access. This change synchronizes access
    to the SPS debugfs buffer.
    
    Signed-off-by: Siva Kumar Akkireddi <sivaa@codeaurora.org>
    (cherry picked from commit de875dd095d3ec0906c77518d28f793e6c69a9da)
    Signed-off-by: Dragan Marinkovic <dmarinkovi@sierrawireless.com>
    Modified-by: Dragan Marinkovic <dmarinkovi@sierrawireless.com>
    Resolves: LXSWIREF-92
    Change-Id: I409b3f0618f760cb67eba47b43c81d166cdae4aa

--- drivers/platform/msm/sps/spsi.h
+++ drivers/platform/msm/sps/spsi.h
@@ -47,26 +47,17 @@ extern u8 debug_level_option;
 extern u8 print_limit_option;
 
 #define MAX_MSG_LEN 80
-#define SPS_DEBUGFS(msg, args...) do {					\
-		char buf[MAX_MSG_LEN];		\
-		snprintf(buf, MAX_MSG_LEN, msg"\n", ##args);	\
-		sps_debugfs_record(buf);	\
-	} while (0)
 #define SPS_ERR(msg, args...) do {					\
 		if (unlikely(print_limit_option > 2))	\
 			pr_err_ratelimited(msg, ##args);	\
 		else	\
 			pr_err(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #define SPS_INFO(msg, args...) do {					\
 		if (unlikely(print_limit_option > 1))	\
 			pr_info_ratelimited(msg, ##args);	\
 		else	\
 			pr_info(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #define SPS_DBG(msg, args...) do {					\
 		if ((unlikely(logging_option > 1))	\
@@ -77,8 +68,6 @@ extern u8 print_limit_option;
 				pr_info(msg, ##args);	\
 		} else	\
 			pr_debug(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #define SPS_DBG1(msg, args...) do {					\
 		if ((unlikely(logging_option > 1))	\
@@ -89,8 +78,6 @@ extern u8 print_limit_option;
 				pr_info(msg, ##args);	\
 		} else	\
 			pr_debug(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #define SPS_DBG2(msg, args...) do {					\
 		if ((unlikely(logging_option > 1))	\
@@ -101,8 +88,6 @@ extern u8 print_limit_option;
 				pr_info(msg, ##args);	\
 		} else	\
 			pr_debug(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #define SPS_DBG3(msg, args...) do {					\
 		if ((unlikely(logging_option > 1))	\
@@ -113,8 +98,6 @@ extern u8 print_limit_option;
 				pr_info(msg, ##args);	\
 		} else	\
 			pr_debug(msg, ##args);	\
-		if (unlikely(debugfs_record_enabled))	\
-			SPS_DEBUGFS(msg, ##args);	\
 	} while (0)
 #else
 #define	SPS_DBG3(x...)		pr_debug(x)
